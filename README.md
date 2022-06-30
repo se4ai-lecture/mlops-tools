0e97e40fa84247a085a0bb87f0be5d1f39521623
Justus Bogner <justus.bogner@iste.uni-stuttgart.de>
2022-06-30 15:32:49 +0200

    Final draft for repo

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Using MLOps Tools
 
-(You can work in pairs, if you want to.)
+(You can work in pairs if you want to.)
+
+This is the base repository you should use for your own GitHub repository (not a TIK Enterprise GitHub, but [Microsoft's public GitHub instance](https://github.com/)).
+
+1. Copy these files into the local directory of your own GitHub repo.
+2. Install the dependencies (`pip install -r requirements.txt`).
+3. Import the needed dataset with DVC.
+4. Extend the script (`src/train.py`) with the necessary MLflow experiment tracking and model storing code (see `TODO` comments).
+5. Run the script (`python src/train.py`) and look at the results in the MLflow UI.
+6. Create a CI pipeline with GitHub Actions.
---
+++ src/train.py
@@ -0,0 +1,72 @@
+# adapted from https://github.com/mlflow/mlflow/blob/master/examples/sklearn_elasticnet_wine/train.py
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+import mlflow
+import mlflow.sklearn
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+def eval_metrics(actual, pred):
+    # Root Mean Square Error (https://en.wikipedia.org/wiki/Root-mean-square_deviation)
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    # Mean Absolute Error (https://en.wikipedia.org/wiki/Mean_absolute_error)
+    mae = mean_absolute_error(actual, pred)
+    # R squared / Coefficient of Determination (https://en.wikipedia.org/wiki/Coefficient_of_determination)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+# set fixed random seed for numpy for reproducibility
+np.random.seed(40)
+
+# read the csv file from the local directory (needs to have been imported / pulled with DVC)
+try:
+    data = pd.read_csv("winequality-red.csv", sep=";")
+except Exception as e:
+    logger.exception(
+        "Unable to find CSV file! Have you imported it with DVC? Error: %s", e
+    )
+    exit(-1)
+
+# split the data into training and test sets (0.75 and 0.25)
+train, test = train_test_split(data)
+
+# the column to be predicted is "quality" which is a scalar from [3, 9]
+train_x = train.drop(["quality"], axis=1)
+test_x = test.drop(["quality"], axis=1)
+train_y = train[["quality"]]
+test_y = test[["quality"]]
+
+# start training loop to test 10 different hyperparameter settings
+for x in range(10):
+
+    # set hyperparameters based on loop variable (0.01 to 0.91)
+    alpha = 0.01 + x / 10
+    l1_ratio = 0.01 + x / 10
+
+    with mlflow.start_run():
+        # instantiate the model
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        # train the model with the training data
+        lr.fit(train_x, train_y)
+        # evaluate the model on the test data
+        predicted_qualities = lr.predict(test_x)
+        # calculate evaluation metrics based on prediction results
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("Training loop #%s" % (x + 1))
+        print("  Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("    RMSE: %s" % rmse)
+        print("    MAE: %s" % mae)
+        print("    R2: %s" % r2)
+
+        # TODO: log the two experiment params and the three evaluation metrics using mlflow
+
+        # TODO: store the model using mlflow
